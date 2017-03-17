3145fd4b060e7828b61a24b8f4b486ee53c15a8c
Sushank Jadhav <sushank.jadhav.prdxn@gmail.com>
2017-03-17 19:11:50 +0530

    Eliminated unused code for better understandability

--- server.js
+++ server.js
@@ -1,45 +1,13 @@
-/* server side dependencies */
-var express = require('express');        // call express
-var path = require('path');
-var morgan = require('morgan');
-var compression = require('compression');
-var helmet = require('helmet');
-var http = require('http');
-var fs = require('fs');
-var robots = require('express-robots');
-var React = require('react');
-
 /* Packages used for Linkedin Functionality */
+var express = require('express');        // call express
 var qs = require('qs');
 var request = require('superagent');
 var bodyParser = require('body-parser');
 
-const psqStageKey = fs.readFileSync('./leadgen-local-key.pem');
-const psqStageCert = fs.readFileSync('./leadgen-local-cert.pem');
-const httpsPort = 8081;
-
-const options = {
-  'key': psqStageKey,
-  'cert': psqStageCert,
-  'ca': [ fs.readFileSync('leadgen-local-cert.pem') ],
-  'checkServerIdentity': function (host, cert) {
-    return undefined;
-  }
-};
-
 /* create app and port initialization */
 const port = process.env.PORT || 8080;
 const app = express();                 // define our app using express
-const router = express.Router();
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ 'extended': false }));
-
-// parse application/json
-app.use(bodyParser.json());
-
-/* serve up content from public directory */
-app.use(express.static(path.join(__dirname, 'dist')));
+const router   = express.Router();
 
 /* Route for getting linkedin AccessToken */
 app.post('/linkedinaccesstoken', function (req, res) {
@@ -73,7 +41,6 @@ app.post('/linkedinaccesstoken', function (req, res) {
       console.log("accessToken---->", accessToken);
       res.send(accessToken);
   });
-
 });
 
 /* Route for getting linkedin AccessToken */
@@ -103,29 +70,3 @@ app.post('/linkedinuserdetails', function (req, res) {
 });
 
 module.exports = router;
-
-/* apply robot.txt file */
-app.use(robots(path.join(__dirname + '/robot.txt')));
-
-/* print all request console */
-app.use(morgan(':method :url :date :remote-addr :status :response-time'));
-
-app.use(compression());
-
-/* security middleware */
-app.use(helmet());
-
-/* handle every other route with index.html, which will contain
-a script tag to your application's JavaScript file(s). */
-app.get('*', function (req, resp){
-  resp.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
-});
-
-/* start http server */
-app.listen(port, '0.0.0.0', (err) => {
-  if (err) {
-    console.log(err);
-  }
-
-  console.info('Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', port, port);
-});
