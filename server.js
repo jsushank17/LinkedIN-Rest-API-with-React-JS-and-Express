8e7f7f9d47675fdb8e71b233b46aadf76baf544f
Sushank Jadhav <sushank.jadhav.prdxn@gmail.com>
2017-03-20 13:07:52 +0530

    Code added for starting the server in case user using whole server.js file in the package

--- server.js
+++ server.js
@@ -70,3 +70,12 @@ app.post('/linkedinuserdetails', function (req, res) {
 });
 
 module.exports = router;
+
+/* start http server */
+app.listen(port, '0.0.0.0', (err) => {
+  if (err) {
+    console.log(err);
+  }
+
+  console.info('Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', port, port);
+});
